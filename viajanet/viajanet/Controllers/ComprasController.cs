b01f84f20cb03036090ccfe668fc35435a8e4f45
Jeferson <jefersonoliver7@gmail.com>
2015-12-26 21:44:14 -0200

    Feita a parte de confirmação de pagamento,

--- viajanet/viajanet/Controllers/ComprasController.cs
+++ viajanet/viajanet/Controllers/ComprasController.cs
@@ -14,8 +14,9 @@ namespace viajanet.Controllers
     public class ComprasController : Controller
     {
         private ViajanetContext db = new ViajanetContext();
-        
-        //
+
+        // get  método que receberá a id da viagem que o usuário selecionou e pegará os hoteis da cidade da viagem pra exibir
+       
         public ActionResult hospedagemHoteis()
         {
             int idCidadeViagem = Convert.ToInt32(Request["cidade"]);
@@ -23,10 +24,38 @@ namespace viajanet.Controllers
 
             TempData["idViagem"] = id;
 
-            var hoteis = db.Hotel.Where(h => h.FK_Cidade == idCidadeViagem);
+            var hoteis = db.Hotel.Include(h => h.Cidade).Include(h => h.Estado).Where(h => h.FK_Cidade == idCidadeViagem);
             
             return View(hoteis.ToList());
         }
+
+        // método que irá conferir se o usuário deseja a hospedagem 
+
+        public ActionResult confirmHospedagem()
+        {
+            if (!string.IsNullOrEmpty(Request["hotel"]) || !string.IsNullOrEmpty(Request["viagem"])) // isso me fará saber se o usuário reservou hotel ou não
+            {
+                var idHotel = Convert.ToInt32(Request["hotel"]);
+                var idViajem = Convert.ToInt32(Request["viagem"]);
+
+                
+                ViewBag.Viajem = db.Viajem.Find(idViajem);
+                var cidade = ViewBag.Viajem.Cidade;
+                ViewBag.Hotel = db.Hotel.Find(idHotel);
+
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(Request["viagem"]))
+                {
+                    var idViagem = Convert.ToInt32(Request["viagem"]);
+                    ViewBag.Viagem = db.Viajem.Find(idViagem);
+                }
+            }
+            return View();
+        }
+
+
 
         // GET: Compras
         public async Task<ActionResult> Index()
@@ -136,6 +165,33 @@ namespace viajanet.Controllers
             db.Compra.Remove(compra);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        // POST: /Create
+        [HttpPost]
+        public ActionResult confirmCompra([Bind(Include = "Id,Fk_Usuario,FK_Viajem,FK_Hotel,Qtd_Adultos,Qtd_Criancas,Valor_Total")] FormCollection collection)
+        {
+            try
+            {
+                Compra compra = new Compra();  
+                compra.Fk_Usuario = collection["Fk_Usuario"];
+                compra.FK_Viajem = Convert.ToInt32(collection["FK_Viajem"]);
+                if (!string.IsNullOrEmpty(collection["FK_Hotel"])) { // aqui eu vejo se está nulo pois eu não sei se o usuário quiz algum hotel
+                    compra.FK_Hotel = Convert.ToInt32(collection["FK_Hotel"]);
+                }
+                compra.Qtd_Adultos = Convert.ToInt32(collection["Qtd_Adultos"]);
+                compra.Qtd_Criancas = Convert.ToInt32(collection["Qtd_Criancas"]);
+                compra.Valor_Total = Convert.ToDouble(collection["Valor_Total"]);
+
+                db.Compra.Add(compra);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         protected override void Dispose(bool disposing)
